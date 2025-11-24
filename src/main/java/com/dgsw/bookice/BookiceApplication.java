9f671208fdb727b704993ed49eb16added8fe68a
encail <siwon0876@gmail.com>
2025-11-24 20:17:05 +0900

    feat: 👍

---
+++ src/main/java/com/dgsw/bookice/BookiceApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.bookice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookiceApplication.class, args);
+    }
+
+}
